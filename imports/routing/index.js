804c8c72c4f5efe5aca2a465605016c3c52d1ffa
yoki <petras.moil@gmail.com>
2018-10-11 22:36:48 +0300

    problem with redirecting

---
+++ imports/routing/index.js
@@ -0,0 +1,6 @@
+import route from './router.js';
+import Home from '/imports/ui/pages/Home.jsx';
+import Login from '/imports/ui/pages/Users/Login.jsx';
+
+route('/', Home);
+route('/login', Login);
